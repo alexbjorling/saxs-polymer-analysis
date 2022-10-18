1443d26bee297f733115f5cd81f751d9487ba9ff
Alexander Björling <alexander.bjorling@maxiv.lu.se>
2022-10-18 22:40:43 +0200

    Code style cleanup and gitignore

--- Chains.py
+++ Chains.py
@@ -1,12 +1,20 @@
+"""
+Contains the man class to represent chains, perturb their structures,
+check for violations and count bonds.
+"""
+
 import numpy as np
-#import multiprocessing
-#import time
 import copy
 import sys
 
+
 class Chains(object):
-    def __init__(self, number=0, length=0, box=0, maxAngle=np.pi/2, beta=0.0, surface=False, outFile='out.pdb', initialConf=None):
-        # initiate a set of <number> chains of length <length>, spread randomly over a square surface of side <box>
+    def __init__(self, number=0, length=0, box=0, maxAngle=np.pi / 2,
+                 beta=0.0, surface=False, outFile='out.pdb', initialConf=None):
+        """
+        Make and initialize a set of <number> chains of length <length>,
+        spread randomly over a square surface of side <box>.
+        """
         if number > 0:
             self.number = number
             self.length = length
@@ -18,7 +26,8 @@ class Chains(object):
             bonds = np.nan
             # read the initial conformation from pdb
             if initialConf:
-                print "Reading initial conformation from the last frame of " + initialConf + "."
+                print("Reading initial conformation from the last frame of %s."
+                      % initialConf)
                 # go to the first line of the last frame
                 fp = open(initialConf, 'r')
                 i = 0
@@ -34,71 +43,89 @@ class Chains(object):
                     self.coords.append(np.ones((length, 3)))
                     for j in range(length):
                         line = fp.readline()
-                        self.coords[-1][j,:] = np.array(map(float, [line[30:38], line[38:46], line[46:54]]))
-                    fp.readline() # TER
+                        self.coords[-1][j, :] = np.array(
+                            map(float,
+                                [line[30:38], line[38:46], line[46:54]])
+                        )
+                    fp.readline()  # TER
                 fp.close()
             # make a random one
             else:
                 while np.isnan(bonds):
-                    sys.stdout.write("Generating initial arrangement of chains... ")
+                    sys.stdout.write(
+                        "Generating initial arrangement of chains... ")
                     self.coords = []
                     for i in range(number):
                         self.coords.append(np.ones((length, 3)))
-                        self.coords[-1][:,0] = np.random.rand() * box
-                        self.coords[-1][:,1] = np.random.rand() * box
-                        self.coords[-1][:,2] = np.arange(.5, length)
+                        self.coords[-1][:, 0] = np.random.rand() * box
+                        self.coords[-1][:, 1] = np.random.rand() * box
+                        self.coords[-1][:, 2] = np.arange(.5, length)
                     bonds = self.check()
                     if np.isnan(bonds):
                         sys.stdout.write('failed. Trying again.\n')
                     else:
                         sys.stdout.write('success!\n')
-                
+
     def randomRotation(self, number=1):
         for cycle in range(number):
             m = np.random.randint(self.number)
-            n = np.random.randint(self.length-1)
-            thetaMax = self.maxAngle # set the random angle to the same value as the maximum allowed angle - should be the right sort of size
-            tX = (1-2*np.random.rand()) * thetaMax * (1 + self.surface)
-            tY = (1-2*np.random.rand()) * thetaMax * (1 + self.surface)
-            tZ = (1-2*np.random.rand()) * thetaMax * (1 + self.surface)
-            rotX = np.array([[1, 0, 0], [0, np.cos(tX), -np.sin(tX)], [0, np.sin(tX), np.cos(tX)]])
-            rotY = np.array([[np.cos(tY), 0, np.sin(tY)], [0, 1, 0], [-np.sin(tY), 0, np.cos(tY)]])
-            rotZ = np.array([[np.cos(tZ), -np.sin(tZ), 0], [np.sin(tZ), np.cos(tZ), 0], [0, 0, 1]])
+            n = np.random.randint(self.length - 1)
+            # set the random angle to the same value as the maximum
+            # allowed angle - should be the right sort of size:
+            thetaMax = self.maxAngle
+            tX = (1 - 2 * np.random.rand()) * thetaMax * (1 + self.surface)
+            tY = (1 - 2 * np.random.rand()) * thetaMax * (1 + self.surface)
+            tZ = (1 - 2 * np.random.rand()) * thetaMax * (1 + self.surface)
+            rotX = np.array(
+                [[1, 0, 0], [0, np.cos(tX), -np.sin(tX)],
+                 [0, np.sin(tX), np.cos(tX)]]
+            )
+            rotY = np.array(
+                [[np.cos(tY), 0, np.sin(tY)], [0, 1, 0],
+                 [-np.sin(tY), 0, np.cos(tY)]]
+            )
+            rotZ = np.array(
+                [[np.cos(tZ), -np.sin(tZ), 0],
+                 [np.sin(tZ), np.cos(tZ), 0], [0, 0, 1]]
+            )
             rot = rotX.dot(rotY).dot(rotZ)
-            origin = self.coords[m][n+1, :]
+            origin = self.coords[m][n + 1, :]
             self.oldCoords = copy.deepcopy(self.coords)
-            for i in range(n+2, self.length):
-                self.coords[m][i,:] = origin + np.dot(rot, self.coords[m][i,:]-origin)
-                
+            for i in range(n + 2, self.length):
+                self.coords[m][i, :] = (
+                    origin + np.dot(rot, self.coords[m][i, :] - origin))
+
     def restorePrevious(self):
-        if self.oldCoords == None:
+        if self.oldCoords is None:
             raise RuntimeError('No saved state to restore')
         else:
             self.coords = self.oldCoords
             self.oldCoords = None
-            
+
     def check(self):
         bonds = 0
-        # got to loop over chains first, beads i and i+1 should interact fully if they are on different chains...
+        # got to loop over chains first, beads i and i+1 should interact
+        # fully if they are on different chains...
         for m in range(self.number):
             # check for angle violation on the current chain:
-            for i in range(1, self.length-1):
-                a = self.coords[m][i+1,:] - self.coords[m][i,:]
-                b = self.coords[m][i,:] - self.coords[m][i-1,:]
-                angle = np.arccos( min(1, np.dot(a,b) / (np.linalg.norm(a)*np.linalg.norm(b)) ))
+            for i in range(1, self.length - 1):
+                a = self.coords[m][i + 1, :] - self.coords[m][i, :]
+                b = self.coords[m][i, :] - self.coords[m][i - 1, :]
+                angle = np.arccos(min(1, np.dot(a, b) / (np.linalg.norm(a)
+                                  * np.linalg.norm(b))))
                 if angle > self.maxAngle:
                     return np.nan
             # check for overlap and surface violation on the current chain:
             for i in range(self.length):
                 ri = self.coords[m][i]
-                if self.surface and (i>0) and (ri[2] < 0.5):
+                if self.surface and (i > 0) and (ri[2] < 0.5):
                     return np.nan
-                for j in range(i-1):
+                for j in range(i - 1):
                     rj = self.coords[m][j]
-                    if np.linalg.norm(ri-rj) < 1.0:
+                    if np.linalg.norm(ri - rj) < 1.0:
                         return np.nan
                     # count bonds
-                    if np.linalg.norm(ri-rj) < 1.2:
+                    if np.linalg.norm(ri - rj) < 1.2:
                         bonds += 1
             # mn cross terms:
             for n in range(m):
@@ -107,29 +134,35 @@ class Chains(object):
                     ri = self.coords[m][i]
                     for j in range(self.length):
                         rj = self.coords[n][j]
-                        if np.linalg.norm(ri-rj) < 1.0:
+                        if np.linalg.norm(ri - rj) < 1.0:
                             return np.nan
                         # count bonds
-                        if np.linalg.norm(ri-rj) < 1.2:
+                        if np.linalg.norm(ri - rj) < 1.2:
                             bonds += 1
         return bonds
-            
+
     def dump(self, append=False):
-        fp = open(self.outFile, {True:'a', False:'w'}[append])
+        fp = open(self.outFile, {True: 'a', False: 'w'}[append])
         fp.write('MODEL \n')
         for j in range(self.number):
             for i in range(self.length):
-                if i == 0: 
+                if i == 0:
                     name = 'A'
                 elif i == 1:
                     name = 'B'
                 else:
                     name = 'C'
                 i_ = j * self.length + i
-                fp.write("ATOM    %3d    %s AAA A %3d    %8.3f%8.3f%8.3f\n"%(i_, name, j, self.coords[j][i,0], self.coords[j][i,1], self.coords[j][i,2]))
+                fp.write(
+                    "ATOM    %3d    %s AAA A %3d    %8.3f%8.3f%8.3f\n"
+                    % (i_, name, j,
+                       self.coords[j][i, 0],
+                       self.coords[j][i, 1],
+                       self.coords[j][i, 2])
+                )
             fp.write('TER   \n')
         fp.write('ENDMDL\n')
         fp.close()
-        
+
     def copy(self):
         return copy.deepcopy(self)
--- chainSimulation.py
+++ chainSimulation.py
@@ -1,5 +1,6 @@
-#!/usr/bin/python
-# -*- coding: utf-8 -*-
+"""
+Main script which runs simulations with command-line argument parameters.
+"""
 
 import numpy as np
 import os
@@ -7,51 +8,73 @@ import Chains
 import time
 import sys
 
+
 # usage
 def usage():
-    b = '\n                        '
-    print "\nUsage:\n"
-    print "chainSimulation.py"
-    print "   -surface:         if present, simulates the chains grafted on a hard but" + b + "inert surface (default no)"
-    print "   -number <n>:      simulates a collection of <n> chains (default 1)"
-    print "   -length <n>:      makes coils of <n> beads, with contour lengths <n>-1" + b + "(default 50)"
-    print "   -box <d>:         grafts the chains on a square of side length <d>" + b + "(default 10)"
-    print "   -maxAngle <a>:    the maximum bend angle at each bead in degrees where" + b + "<a>=0.0 is a straight rod (default 90.0)"
-    print "   -beta <b>:        the effective bond strength <b> = energy / (kT), where" + b + "the well width is 0.2 (default 0.0)"
-    print "   -ramps <n>:       ramp up beta from 0 <n> times for simulated annealing, " + b + "where <n> = 0 gives constant beta as specified by" + b + "-beta. After each ramp, beta is held constant for the" + b + "equivalent of a ramp duration. (default 0)"
-    print "   -outputFile <s>:  the base name of the outputfile (default: 'out')"
-    print "   -outputFreq <n>:  produces output every <n> simulation steps (default: 10)"
-    print "   -steps <n>:       runs a simulation of <n> steps, including steps where the" + b + "change is rejected (default 1000)"
-    print "   -append:          if present, continues the existing simulation specified by" + b + "-outputFile, which must match the other settings" + b + "(default no)"
-    print ""
+    msg = (
+        '\nUsage:\n\n'
+        'chainSimulation.py\n\n'
+        '  -surface:         if present, simulates the chains grafted on a\n'
+        '                    hard but inert surface (default no)\n'
+        '  -number <n>:      simulates a collection of n chains (default 1)\n'
+        '  -length <n>:      makes coils of n beads, with contour lengths\n'
+        '                    n - 1 (default 50)\n'
+        '  -box <d>:         grafts the chains on a square of side length d\n'
+        '                    (default 10)\n'
+        '  -maxAngle <a>:    the maximum bend angle at each bead in degrees\n'
+        '                    where a = 0.0 is a straight rod (default 90.0)\n'
+        '  -beta <b>:        the effective bond strength b = energy / (kT),\n'
+        '                    where the well width is 0.2 (default 0.0)\n'
+        '  -ramps <n>:       ramp up beta from 0 n times for simulated\n'
+        '                    annealing, where n = 0 gives constant beta as\n'
+        '                    specified by -beta. After each ramp, beta is\n'
+        '                    held constant for the equivalent of a ramp\n'
+        '                    duration. (default 0)\n'
+        '  -outputFile <s>:  the base name of the outputfile (default "out")\n'
+        '  -outputFreq <n>:  produces output every n simulation steps \n'
+        '                    (default: 10)\n'
+        '  -steps <n>:       runs a simulation of n steps, including steps\n'
+        '                    where the change is rejected (default 1000)\n'
+        '  -append:          if present, continues the existing simulation\n'
+        '                    specified by -outputFile, which must match the\n'
+        '                    other settings (default no)\n'
+    )
+    print msg
     exit()
 
-# parse arguments
-if '-help' in sys.argv: usage()
-surface = '-surface' in sys.argv
-append = '-append' in sys.argv
+
 def parse(argv, key, default):
     if key in argv:
         return argv[argv.index(key) + 1]
     else:
         return default
-number     = int(parse(sys.argv, '-number', 1))
-length     = int(parse(sys.argv, '-length', 50))
-box        = float(parse(sys.argv, '-box', 10))
-maxAngle   = float(parse(sys.argv, '-maxAngle', 90)) * np.pi / 180.0
-beta       = float(parse(sys.argv, '-beta', 0))
-ramps      = int(parse(sys.argv, '-ramps', 0))
+
+
+# parse arguments
+if '-help' in sys.argv:
+    usage()
+surface = '-surface' in sys.argv
+append = '-append' in sys.argv
+number = int(parse(sys.argv, '-number', 1))
+length = int(parse(sys.argv, '-length', 50))
+box = float(parse(sys.argv, '-box', 10))
+maxAngle = float(parse(sys.argv, '-maxAngle', 90)) * np.pi / 180.0
+beta = float(parse(sys.argv, '-beta', 0))
+ramps = int(parse(sys.argv, '-ramps', 0))
 outputFile = parse(sys.argv, '-outputFile', 'out')
 outputFreq = int(parse(sys.argv, '-outputFreq', 10))
-nSteps     = int(parse(sys.argv, '-steps', 1000))
+nSteps = int(parse(sys.argv, '-steps', 1000))
 if (number > 1) and not surface:
     print "Simulating more than one chain without a surface makes no sense!\n"
     exit()
 
 # start the timer and initialize the Chains instance
 t0 = time.time()
-initialConf = {False: None, True: outputFile+'.pdb'}[append]
-chains = Chains.Chains(number=number, length=length, box=box, maxAngle=maxAngle, beta=beta, surface=surface, outFile=outputFile+'.pdb', initialConf=initialConf)
+initialConf = {False: None, True: outputFile + '.pdb'}[append]
+chains = Chains.Chains(
+    number=number, length=length, box=box, maxAngle=maxAngle, beta=beta,
+    surface=surface, outFile=outputFile + '.pdb', initialConf=initialConf,
+)
 
 # set up the output
 fp = open(outputFile + '.commandLine', 'w')
@@ -60,8 +83,10 @@ for s in sys.argv:
 fp.write('\n')
 fp.close()
 if not append:
-    try: os.remove(outputFile + '.pdb')
-    except: pass
+    try:
+        os.remove(outputFile + '.pdb')
+    except:
+        pass
     chains.dump(append=True)
 
 # do the simulation
@@ -81,7 +106,7 @@ for i in range(nSteps):
     # Metropolis condition:
     if np.isnan(bonds):
         keep = False
-    elif (deltaBonds <= 0) or (np.random.rand() < np.exp(-beta_*deltaBonds)):
+    elif (deltaBonds <= 0) or (np.random.rand() < np.exp(-beta_ * deltaBonds)):
         keep = True
     else:
         keep = False
@@ -97,12 +122,16 @@ for i in range(nSteps):
     i_ = i + 1
     if (i_ % outputFreq == 0) or (i_ == nSteps):
         t = time.time()
-        betaText  = ''
+        betaText = ''
         if ramps:
-            betaText = ', beta=%.2f'%beta_
-        remaining = float(nSteps-i_)*((t-t0))/i_
-        timeString = {True:'%.1fs'%remaining, False:'%.0fmin'%(remaining//60,)}[remaining<300]
-        print '   step %d/%d: %.1fs, %s remaining'%(i_, nSteps, t-t0, timeString) + betaText
+            betaText = ', beta=%.2f' % beta_
+        remaining = float(nSteps - i_) * ((t - t0)) / i_
+        if remaining < 300:
+            timeString = '%.1fs' % remaining
+        else:
+            '%.0fmin' % (remaining // 60,)
+        print '   step %d/%d: %.1fs, %s remaining' % (
+            i_, nSteps, t - t0, timeString) + betaText
         chains.dump(append=True)
 
 # fix a nice vmd file for this simulation
@@ -117,9 +146,13 @@ fout.close()
 fin.close()
 
 t = time.time()
-timeText = {True: '%.1fs'%(t-t0), False: '%.0fmin'%((t-t0)/60)}[t-t0 < 300]
+if t - t0 < 300:
+    timeText = '%.1fs' % (t - t0)
+else:
+    timeText = '%.0fmin' % ((t - t0) / 60)
 print '\nSimulation done in ' + timeText
-print 'Trajectory written to %s.pdb'%outputFile
-print 'Visualization state saved as %s.vmd, do "vmd -e %s.vmd" to look'%((outputFile,)*2)
+print 'Trajectory written to %s.pdb' % outputFile
+print('Visualization state saved as %s.vmd, do "vmd -e %s.vmd" to look'
+      % ((outputFile,) * 2))
 if nSteps > 0:
-    print 'Acceptance rate: %.1f%%'%(100*float(goodSteps)/nSteps)
+    print 'Acceptance rate: %.1f%%' % (100 * float(goodSteps) / nSteps)
